7a355d571c5a3b2ac03b6c9ac8791523c74a59da
Hector Trillo <htrillo95@gmail.com>
2024-05-06 21:16:07 -0400

    task 3 update

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Job {
 
+    //TASK #3 FIELDS GIVEN - STARTER CODE
     private int id;
     private static int nextId = 1;
 
@@ -22,4 +23,81 @@ public class Job {
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
+    //TASK #3 - CONSTRUCTORS
+
+    //Constructor #1 - initialize unique ID
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    //Constructor #2 - initialize fields & call first constructor
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this(); // Calls the first constructor to initialize the 'id' field
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    //TASK #3 - SETTERS & GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    //TASK #3 - GENERATE EQUALS METHOD & HASHCODE
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
